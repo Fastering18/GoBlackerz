b9796ff62401a01b4e3a2232f37bf28487755cc7
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-06-04 08:58:40 +0700

    Update blackerz.go

--- blackerz.go
+++ blackerz.go
@@ -1,6 +1,20 @@
 package blackerz
 
+import (
+   "io/ioutil"
+   "log"
+   "net/http"
+)
+
 // get bot data
-func GetBotData(name string) string {
-    return "test"
+func GetBotData(id string) string {
+    resp, err := http.Get("https://blackerz.herokuapp.com/api/v1/bots/"+id)
+    if err != nil {
+      log.Fatalln(err)
+    }
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+      log.Fatalln(err)
+    }
+    return string(body)
 }
