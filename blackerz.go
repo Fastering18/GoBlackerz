5c7d71584ab97d3cdfab7e1a4bb838cf94b16b96
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-06-05 00:43:50 +0700

    Update blackerz.go

--- blackerz.go
+++ blackerz.go
@@ -4,10 +4,30 @@ import (
    "io/ioutil"
    "log"
    "net/http"
+   "encoding/json"
 )
 
+type Bot struct {
+    LongDescription string `json:"LongDescription"`
+    ShortDescription string `json:"ShortDescription"`
+    Avatar string `json:"avatar"`
+    InviteLink string `json:"inviteLink"`
+    Name string `json:"name"`
+    Owner BotOwner `json:"owner"`
+    Prefix string `json:"prefix"`
+    ServerCount int `json:"serverCount"`
+    Tag string `json:"tag"`
+    Upvotes int `json:"upvotes"`
+    Verified bool `json:"verified"`
+    ID string `json:"id"`
+}
+type BotOwner struct {
+    ID string `json:"id"`
+    Name string `json:"name"`
+}
+
 // get bot data
-func GetBotData(id string) string {
+func GetBotData(id string) Bot {
     resp, err := http.Get("https://blackerz.herokuapp.com/api/v1/bots/"+id)
     if err != nil {
       log.Fatalln(err)
@@ -16,5 +36,7 @@ func GetBotData(id string) string {
     if err != nil {
       log.Fatalln(err)
     }
-    return string(body)
+    var data Bot;
+    json.Unmarshal(body, &data)
+    return data
 }
